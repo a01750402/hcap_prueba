c97c6638f54f8ae7577f3ea18d8a68667f8e9b11
santior411 <santior411@DESKTOP-VRUQ41H.localdomain>
2020-10-30 08:31:23 -0600

    Agregar cambios realizados

--- myconvolution.py
+++ myconvolution.py
@@ -7,6 +7,7 @@ def convolution(A,B):
     suma = 0 
     for i in range(len(C)):
         for j in range(len(C[0])):
+            suma = 0 
             for iaux in range(len(B)):
                 for jaux in range(len(B[0])):
                     suma += A[i + iaux][j + jaux]*B[iaux][jaux]
@@ -20,7 +21,11 @@ A = np.array(Matrix1)
 B = np.array(Filtro)
 #C = np.zeros((2,2))
 convolution(A,B)
-
+Matrix2 = [[1,2,3,4,5,6],[7,8,9,10,11,12],[0,0,1,16,17,18],[0,1,0,7,23,24],[1,7,6,5,4,3]]
+Filtro2 = [[1,1,1],[0,0,0],[2,10,3]]
+D = np.array(Matrix2)
+E = np.array(Filtro2)
+print(convolution(D,E))
 #print(Matrix1)
 print(A)
 print(A[1][0]) 
